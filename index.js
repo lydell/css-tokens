95e3c9fd92fd077b42d46129584561336298e7d2
Simon Lydell <simon.lydell@gmail.com>
2015-06-20 08:38:39 +0200

    Declare undeclared variable

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 module.exports = /((['"])(?:(?!\2)[^\\\r\n\f]|\\(?:\r\n|[\s\S]))*(\2)?)|(\/\*(?:[^*]|\*(?!\/))*(\*\/)?)|([+-]?\d*\.?\d+(?:[eE][+-]?\d+)?)|(url\((?!\s*["'])\s*(?:[^"'()\\\s]|\\(?:[\da-fA-F]{1,6}\s?|[^\r\n\f]))*(?:\s*\))?)|((?:[@.]?(?!-?\d)|\#)(?!-+(?![\w\-\u0080-\uFFFF\\]))(?:[\w\-\u0080-\uFFFF]|\\(?:[\da-fA-F]{1,6}\s?|[^\r\n\f]))+)|([~|^$*]?=|[>~+*\/]|-|[|[\](){},;!%]|::?)|([ \t\n\r\f]+)|(^$|[\s\S])/g
 
 module.exports.matchToToken = function(match) {
-  token = {type: "invalid", value: match[0]}
+  var token = {type: "invalid", value: match[0]}
        if (match[ 1]) token.type = "string" , token.closed = !!match[3]
   else if (match[ 4]) token.type = "comment", token.closed = !!match[5]
   else if (match[ 6]) token.type = "number"
