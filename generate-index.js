c22f5302edb17d244a1398ab343b9ef37ae0624b
Simon Lydell <simon.lydell@gmail.com>
2015-06-20 08:53:59 +0200

    Fix build when using newer V8 versions

--- generate-index.js
+++ generate-index.js
@@ -7,5 +7,14 @@ require("coffee-script/register")
 var regex = require("./regex.coffee")
 
 var code = fs.readFileSync("index.js").toString()
-code = code.replace(/\/.+\/.+/, "/" + regex.source.replace(/\//g, "\\/") + "/g")
+
+var regexString = ""
+// Older V8 versions are buggy and don’t escape slashes correctly.
+if (RegExp("/").source === "/") {
+  regexString = "/" + regex.source.replace(/\//g, "\\/") + "/g"
+} else {
+  regexString = regex.toString()
+}
+
+code = code.replace(/\/.+\/.+/, regexString)
 fs.writeFileSync("index.js", code)
